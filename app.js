b835d2037d70b7183350cf8632182835c67ac4a4
Vinodhbabu Dharani <vinodh.dharani@gmail.com>
2017-01-08 12:41:11 -0600

    support for HTTPS

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
           .controller('NarrowItDownController', NarrowItDownController)
           .service('MenuSearchService', MenuSearchService)
           .directive('foundItems', FoundItems)
-          .constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
+          .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
 
   NarrowItDownController.$inject = ['MenuSearchService'];
   MenuSearchService.$inject = ['$http', 'ApiBasePath'];
